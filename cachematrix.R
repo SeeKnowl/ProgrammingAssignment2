356ab448ad831b410b9a74218ab8694344ec6210
SeeKnowl <SeeKnowl@users.noreply.github.com>
2015-05-23 16:37:07 -0700

    R Prog Assignment 2: Bring in the code for makeCacheMatrix function
    Programming Assignment 2: Bring in the tested code from local RStudio
    into the local clone for the function makeCacheMatrix.  Also testing the
    GitHub commit for the first time

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,40 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## Function name: makeMatrix
+## 
+## Argument: object type of Matrix
+## 
+## Purpose: function that defines & returns a list of four functions
+##  Function to get the matrix from Cached memory object
+##  Function to set the matrix into memory cache
+##  Functions to set (setInverse) and get (getInverse) the cached Matrix Inverse
+
+	## initialize the matrix
+ 	m <- NULL
+  
+  	## Set the value of the global cached object - the matrix to be inversed
+  	## set the value of the global Cached Matrix Inverse object to null
+  
+  	set <- function(y) {
+   	 	x <<- y
+    		m <<- NULL
+  	}
+  
+  	## get the cached matrix - the matrix to be inversed
+  
+  	get <- function() x
+  
+  	## set the global cached matrix inverse object with the given object
+  	setInverse <- function(MatrixInverse) m <<- MatrixInverse
+  
+  	## return the stored global cached matrix inverse 
+  	getInverse <- function() m
+  
+  	## retrun the list of the four functions defined above
+  	list(set = set, get = get,
+       	setInverse = setInverse,
+       	getInverse = getInverse)
 }
 
 
